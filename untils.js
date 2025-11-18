cc9187706f24be8bb71ee73eda62b2d66629df99
gilangRusli <finedud8@gmail.com>
2025-11-18 22:02:44 +0700

    Delete untils.js

--- untils.js
+++
@@ -1,9 +0,0 @@
-function formatRupiah(angka) {
-  return "Rp " + angka.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-function isValidNumber(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-module.exports = { formatRupiah, isValidNumber};
