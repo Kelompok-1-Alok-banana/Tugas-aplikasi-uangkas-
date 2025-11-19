9f20474148d45b27a550ffbba52abce641040197
gilang <finedud8@gmail.com>
2025-11-19 10:20:47 +0700

    gilang

--- uangkas.js
+++ uangkas.js
@@ -1,1 +1,9 @@
+const prompt = require("prompt-sync")();
 
+let data = [];
+
+function inputPemasukan() {
+    const nama = prompt("Nama: ");
+    const jumlah = parseInt(prompt("Jumlah: "));
+    data.push({ tipe: "uang", nama, jumlah });
+}
